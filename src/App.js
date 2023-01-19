86aad4dc741182b587845a818629c434fa1697a7
akilasonti <akilasonti9@gmail.com>
2023-01-19 22:03:51 +0530

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { FaArrowRight } from 'react-icons/fa'
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState('')
+  const [shortenedLink, setShortenedLink] = useState('')
+
+  const fetchData = async () => {
+    try {
+      const response = await axios(
+        `https://api.shrtco.de/v2/shorten?url=${input}`
+      )
+      setShortenedLink(response.data.result.full_short_link)
+      setInput('')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='title'>Link Shortner</h1>
+      <div className='form'>
+        <input
+          type='text'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          autoFocus
+          placeholder='Enter Your Link'
+        />
+        <button className='btn submit-btn' onClick={() => fetchData()}>
+          <FaArrowRight />
+        </button>
+      </div>
+      <br />
+      {shortenedLink}
+      <CopyToClipboard text={shortenedLink}>
+        <button className='btn copy-btn'>COPY URL to Clipboard</button>
+      </CopyToClipboard>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
